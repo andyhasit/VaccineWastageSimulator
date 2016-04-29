eed0b48a63e1b6854338fd324ca04d6aa6f1c2c7
Andrew Buchan <andyhasit@gmail.com>
2016-04-29 18:01:30 +0100

    SPLIT TEST FILES

---
+++ tests/Model.test.js
@@ -0,0 +1,57 @@
+
+describe('Model', function() {
+
+  var Model;
+  
+  beforeEach(module('app'));
+  
+  beforeEach(inject(function(_$rootScope_, _Model_) {
+    $rootScope = _$rootScope_;
+    Model = _Model_;
+  }));
+  
+  it('getDataSet', function() {
+    Model.inputs = { 
+      dosesPerYear: 1456,
+      sessionsPerWeek : 4,
+      dosesPerVial: 10
+    };
+    Model.calculateAll();
+    var dataSet = Model.getDataSet();
+    var entry4 = dataSet[4];
+    expect(entry4.dosesAdministered).toEqual(4);
+    expect(entry4.dosesWasted).toEqual(6);
+    expect(entry4.probability).toBeCloseTo(0.091068681, 5);
+    expect(entry4.expectedSessions).toBeCloseTo(18.94228559, 5);
+    expect(entry4.wastageRate).toEqual(0.6);
+    
+    var entry13 = dataSet[13];
+    expect(entry13.dosesAdministered).toEqual(13);
+    expect(entry13.dosesWasted).toEqual(7);
+    expect(entry13.probability).toBeCloseTo(0.01407595, 5);
+    expect(entry13.expectedSessions).toBeCloseTo(2.927797523, 5);
+    expect(entry13.wastageRate).toEqual(0.35);
+  });
+  
+  it('getDataSet random', function() {
+    /*
+    Just checking an issue with 170/171 cuttoff which cause a problem with shortenedFactorial.
+    */
+    Model.inputs = { 
+      dosesPerYear: 169,
+      sessionsPerWeek : 2,
+      dosesPerVial: 5
+    };
+    Model.calculateAll();
+    var dataSet = Model.getDataSet();
+    Model.inputs = { 
+      dosesPerYear: 170,
+      sessionsPerWeek : 2,
+      dosesPerVial: 5
+    };
+    Model.calculateAll();
+    var dataSet = Model.getDataSet();
+  });
+   
+  
+});
--- tests/calculations.test.js
+++ tests/calculations.test.js
@@ -1,14 +1,13 @@
 
 describe('Calculations', function() {
 
-  var Model, Calculations;
+  var Calculations;
   
   beforeEach(module('app'));
   
-  beforeEach(inject(function(_$rootScope_, _Model_, _Calculations_) {
+  beforeEach(inject(function(_$rootScope_, _Calculations_) {
     $rootScope = _$rootScope_;
     Calculations = _Calculations_;
-    Model = _Model_;
   }));
 
   it('calculateVaccinesWastes', function() {
@@ -37,49 +36,5 @@ describe('Calculations', function() {
     expect(Calculations.calculateBinomialDistribution(5, dosesPerYear, probability)).toBeCloseTo(0.12776012, 5);
     expect(Calculations.calculateBinomialDistribution(12, dosesPerYear, probability)).toBeCloseTo(0.026231565, 5);
   });
-  
-  it('getDataSet', function() {
-    Model.inputs = { 
-      dosesPerYear: 1456,
-      sessionsPerWeek : 4,
-      dosesPerVial: 10
-    };
-    Model.calculateAll();
-    var dataSet = Model.getDataSet();
-    var entry4 = dataSet[4];
-    expect(entry4.dosesAdministered).toEqual(4);
-    expect(entry4.dosesWasted).toEqual(6);
-    expect(entry4.probability).toBeCloseTo(0.091068681, 5);
-    expect(entry4.expectedSessions).toBeCloseTo(18.94228559, 5);
-    expect(entry4.wastageRate).toEqual(0.6);
-    
-    var entry13 = dataSet[13];
-    expect(entry13.dosesAdministered).toEqual(13);
-    expect(entry13.dosesWasted).toEqual(7);
-    expect(entry13.probability).toBeCloseTo(0.01407595, 5);
-    expect(entry13.expectedSessions).toBeCloseTo(2.927797523, 5);
-    expect(entry13.wastageRate).toEqual(0.35);
-  });
-  
-  it('getDataSet random', function() {
-    /*
-    Just checking an issue with 170/171 cuttoff which cause a problem with shortenedFactorial.
-    */
-    Model.inputs = { 
-      dosesPerYear: 169,
-      sessionsPerWeek : 2,
-      dosesPerVial: 5
-    };
-    Model.calculateAll();
-    var dataSet = Model.getDataSet();
-    Model.inputs = { 
-      dosesPerYear: 170,
-      sessionsPerWeek : 2,
-      dosesPerVial: 5
-    };
-    Model.calculateAll();
-    var dataSet = Model.getDataSet();
-  });
-   
   
 });
